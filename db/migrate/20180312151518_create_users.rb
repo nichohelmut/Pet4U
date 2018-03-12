e9c895c6a0939616a4a2211392e334e7c3107bbc
karenhoyos <karenuca@hotmail.com>
2018-03-12 16:15:37 +0100

    Feat, added User model

---
+++ db/migrate/20180312151518_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :address
+      t.imteger :role
+      t.text :description
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
